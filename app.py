e4b8966505256b3686d1a687f302c487e45a5068
giovanicorral <giovani.corral@gmail.com>
2023-04-25 21:02:29 -0300

    Adicionando methods POST GET

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for 
 import repositorio
 
 app = Flask(__name__)
@@ -15,15 +15,41 @@ def listagem_produtos():
 
 
 #Abrir um produto específico (carregando seus dados) no template cadastro.html
-@app.route("/produto/<int:id>")
+@app.route("/produto/<int:id>", methods=["GET"])
 def exibir_produto(id):
+    if id == 0: #Estamos verificando se o id recebido foi ZERO
+        id = repositorio.gerar_id() #Caso o id recebido tenha sido ZERO, vamos gerar um novo id, pois é sinal de que estão querendo criar um novo produto (essa combinação de que 0 = novo produto foi geita por nós)
+
     produto = repositorio.retornar_produto(id)
     produto['id'] = id
     return render_template('cadastro.html', **produto)
 
 #Abrir o template cadastro.html apenas com o id preenchido para permitir novo cadastro
 #Dar função aos botões excluir e salvar no template cadastro.html
-    
+@app.route("/produto/<int:id>", methods=["POST"])
+#A linha acima indica que a mesma rota que já haviamos criado antes foi recriada com o metodo POST, ou seja, quando o usuário enviar dados
+def editar_produto(id):
+    if "excluir" in request.form:        
+        #Aqui estamos verificando que "excluir" está contido na requisão, ou seja, o usuário clicou no botão excluir do formulário
+        repositorio.remover_produto(id)
 
+    elif "salvar" in request.form:        
+        #Aqui estamos verificando que "salvar" está contido na requisão, ou seja, o usuário clicou no botão salvar do formulário
+        produto = {} #Criando um dicionário vazio para conter os dados do produto que será salvo
+        produto["nome"] = request.form["nome"] #Colocamos no dicionario o conteudo que veio do formulario
+        produto["descricao"] = request.form["descricao"]
+        produto["preco"] = request.form["preco"]
+        produto["imagem"] = request.form["imagem"]
+
+        #Precisamos definir se vou SALVAR um novo produto ou ATUALIZAR um produto já existente
+        produtos = repositorio.retornar_produtos()
+        #Vamos testar se o id do produto está no dicionário que contem todos eles
+        if id in produtos.keys():
+            repositorio.atualizar_produto(id, produto) #Caso o id já exista, vamos chamar a função atualizar_produto indicando o id e os novos dados
+        else:
+            repositorio.criar_produto(**produto) #Caso a id não existam vamos chamar a função criar_produto passando os dados do dicionário
+            #Podemos fazer da seguinte forma -> repositorio.criar_produto(nome=produto['nome'], descricao=produto['descricao'], preco=produto['preco'], imagem=produto['imagem'])
+    #o nosso RETURN est'a fora dos ifs porque será executado INDEPENDENTEMENTE do botão que for clicado
+    return redirect(url_for('listagem_produtos'))
 
 app.run(debug=True)
